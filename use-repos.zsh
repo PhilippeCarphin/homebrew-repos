8e6a01e94db41a43de710ca46a04991cdbad9337
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-05-27 19:12:21 -0400

    update forzsh

--- use-repos.zsh
+++ use-repos.zsh
@@ -1,19 +1,21 @@
 #!/bin/zsh
 
-this_file=$(python3 -c "import os; print(os.path.realpath('${BASH_SOURCE[0]}'))")
+this_file=$(python3 -c "import os; print(os.path.realpath('$_'))")
 echo "this_file = $this_file"
-this_dir=$(cd $(dirname ${this_file}) && pwd)
 
-echo "this_dir = $this_dir"
-if ! [ -e $this_dir/repos ] ; then
-    echo "Please make sure there is an executable 'repos' in $this_dir."
-    echo "You probably want to do 'mv repos_darwin_amd64 repos' if you are on an amd64'"
+if ! which repos >/dev/null ; then
+    echo "this_dir = $this_dir"
+    if ! [ -e $this_dir/repos ] ; then
+        echo "Please make sure there is an executable 'repos' in $this_dir."
+        echo "You probably want to do 'mv repos_darwin_amd64 repos' if you are on an amd64'"
+        echo "or if you cloned the repo, you would want to build using 'go build .'"
+        return 8
+    fi
+
+    function repos(){
+        ${this_dir}/repos "$@"
+    }
 fi
-
-
-function repos(){
-    ${this_dir}/repos "$@"
-}
 
 function rcd(){
     cd $(repos -get-dir $1)
