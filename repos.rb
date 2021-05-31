8700f737c001091e9c5acd2e5a1415e0478a2233
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-05-31 11:14:45 -0400

    Install completion scripts

--- repos.rb
+++ repos.rb
@@ -19,7 +19,9 @@ class Repos < Formula
     bin.install "repos" => "repos"
     bin.install "scripts/git-recent" => "git-recent"
     man1.install "man/man1/repos.man" => "repos.1"
-    share.install "completions" => "completions"
+    share.install "completions/repos_completion.bash" => "completions/repos_completion.bash"
+    share.install "completions/repos_completion.fish" => "completions/repos_completion.fish"
+    share.install "completions/repos_completion.zsh" => "completions/repos_completion.zsh"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
