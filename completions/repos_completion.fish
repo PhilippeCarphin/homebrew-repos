be1a20d85dcb65322bd4f3973d03747ee849df60
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-06-02 07:06:11 -0400

    Add options to fish completion

--- completions/repos_completion.fish
+++ completions/repos_completion.fish
@@ -3,7 +3,9 @@ function rcd
     echo "[33mcd $dir[0m"
     cd $dir
 end
+complete -f -c rcd -a '(repos -list-names)'
 
 complete -f -c repos -n 'contains -- -r (commandline -opc)' -a '(repos -list-names)'
-
-complete -f -c rcd -a '(repos -list-names)'
+complete -f -c repos  -a '-generate-config' -d 'Generate a config file on STDOUT from repos in PWD'
+complete -f -c repos  -a '-no-fetch' -d 'Skip fetching step'
+complete -f -c repos  -a '-days' -d 'Skip fetching step'
