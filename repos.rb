8531fc69d15f484ae185d875acb546b4b5a3eec4
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-06-10 15:21:06 -0400

    Updated formula for new CMake build

--- repos.rb
+++ repos.rb
@@ -10,27 +10,29 @@ class Repos < Formula
   license ""
 
   depends_on "go" => :build
+  depends_on "cmake" => :build
+  depends_on "pandoc" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "make", "repos"
-    bin.install "repos" => "repos"
-    bin.install "scripts/git-recent" => "git-recent"
-    man1.install "share/man/man1/repos.1" => "repos.1"
-    man1.install "share/man/man1/rcd.1" => "rcd.1"
-    share.install "completions" => "completions"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build", "--target", "install"
+    # bin.install "bin/repos" => "repos"
+    # bin.install "scripts/git-recent" => "git-recent"
+    # man1.install "share/man/man1/repos.1" => "repos.1"
+    # man1.install "share/man/man1/rcd.1" => "rcd.1"
+    # share.install "completions" => "completions"
   end
 
   def caveats
     s = <<~EOS
       Please source one of
 
-        #{HOMEBREW_PREFIX}/share/completions/repos_completion.bash
-        #{HOMEBREW_PREFIX}/share/completions/repos_completion.fish
-        #{HOMEBREW_PREFIX}/share/completions/repos_completion.zsh
+        #{HOMEBREW_PREFIX}/share/completions/repos/repos_completion.bash
+        #{HOMEBREW_PREFIX}/share/completions/repos/repos_completion.fish
+        #{HOMEBREW_PREFIX}/share/completions/repos/repos_completion.zsh
 
       in your ~/.bashrc, ~/.zshrc, or ~/.config/fish/config.fish.
     EOS
