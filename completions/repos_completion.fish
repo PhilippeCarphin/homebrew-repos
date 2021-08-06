29f257ed340d19a0f9df6097dbbe1c67a9dc419f
Philippe Carphin <philippe.carphin2@canada.ca>
2021-08-06 10:25:09 -0400

    Add -get-dir to autocompletes

--- completions/repos_completion.fish
+++ completions/repos_completion.fish
@@ -6,6 +6,7 @@ end
 complete -f -c rcd -a '(repos -list-names)'
 
 complete -f -c repos -n 'contains -- -r (commandline -opc)' -a '(repos -list-names)'
+complete -f -c repos -n 'contains -- -get-dir (commandline -opc)' -a '(repos -list-names)'
 complete -f -c repos  -a '-generate-config' -d 'Generate a config file on STDOUT from repos in PWD'
 complete -f -c repos  -a '-no-fetch' -d 'Skip fetching step'
 complete -f -c repos  -a '-days' -d 'Skip fetching step'
